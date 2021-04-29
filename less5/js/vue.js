c81631ddc3f5b194e874c3787657572cc8a1703e
AncleMarvel <anlcemarvel@gmail.com>
2021-04-29 23:02:39 +0300

    ready

--- less5/js/vue.js
+++ less5/js/vue.js
@@ -1,20 +1,7 @@
-/*  
-    ТЗ:
-    1. Получить JSON данных по продуктам используя API
-    2. Основываясь на этих данных сделать карточки товаров
-    3. Отобразить карточки товаров на странице
-    4. Создать корзину
-    5. По кнопке "купить" добавить товар в корзину
-    5.1 Отобразить показатель кол-ва товаров над корзиной
-    6. В корзине по кнопке "удалить": если кол-во товаров больше 1, то отнять 1; 
-        если равно 1, то убрать разметку из корзины
-    6.1 Если кол-во товаров в корзине > 1, отминусовать от показателя;
-        иначе - скрыть показатель кол-ва товаров в корзине
-
-    7. В поле поиска пользователь вводит строку, ПО КЛИКУ поиска сопостовлять  
-        значение строки с именами продуктов, скрывать все карточки товаров на странице
-        для которых значение поиска не совпадает.
-*/
+/**
+ * Помимо получения данных по апи и отображения их на странице,
+ * Реализовать поиск и удаление/добавление товаров в корзину
+ */
 class Cart {
     products = [];
 }
@@ -47,12 +34,11 @@ class State {
 const shop = new Vue({
     el: "#shop",
     data: {
-        state: new State (new Shop()),
+        state: new State(new Shop()),
         api: "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/",
         productsGetJsonMethod: "catalogData.json",
-        productImg: "https://placehold.it/200x150",
-        productInCartImg: "https://placehold.it/50x100",
-        productsData: [],
+        productImg: "https://via.placeholder.com/200x150",
+        productInCartImg: "https://via.placeholder.com/50x100",
         quantityOfProductsInCart: 0,
         visibleCart: false,
         searchText: "",
@@ -79,17 +65,32 @@ const shop = new Vue({
             const cartItem = this.state.shop.cart.products.find(p => p.product.id_product == product.product.id_product);
             const indexOfRemoveProduct = this.state.shop.cart.products.indexOf(product.product);
             !cartItem ? this.state.shop.cart.products.splice(indexOfRemoveProduct, 1) : cartItem.decreaseQuantity();
+            this.quantityOfProductsInCart--;
+        },
+
+        search() {
+            const productNameList = document.querySelectorAll(".product-name");
+            document.querySelector("#products").childNodes.forEach(el => {
+                if (el.querySelector(".product-name").innerText.toLowerCase().indexOf(this.searchText.toLowerCase()) == -1) {
+                    el.style.display = "none";
+                } else { 
+                    el.style.display = "block";
+                }
+            });
+
+            if (this.searchText == "") {
+                document.querySelector("#products").childNodes.forEach(el => {
+                    el.style.display = "block";
+                });
+            }
         }
     },
 
     mounted() {
         this.getJson(this.productsGetJsonMethod)
             .then(pData => {
-                this.productsData = pData;
-                console.dir(this.productsData);
+                this.sourceData = pData;
             });
-
-        // const state = new State(new Shop());
     }
 });
 
