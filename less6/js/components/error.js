7637cdee443c1266ee0b171e6e142b397f12da4d
AncleMarvel <anlcemarvel@gmail.com>
2021-07-06 17:04:51 +0300

    start working on error comp

---
+++ less6/js/components/error.js
@@ -0,0 +1,12 @@
+Vue.component('error', {
+    props: ['message'],
+    data() {
+        return {
+            
+        }
+    },
+    template: ``,
+    methods: {
+
+    }
+});
--- less6/js/components/search.js
+++ less6/js/components/search.js
@@ -6,11 +6,11 @@ Vue.component('search', {
     },
     template:
         `<form action="#" class="search-form" id="search">
-        <input type="text" class="search-field" v-model.lazy="searchText">
-        <button @click.prevent="search()" class="btn-search" type="submit">
-            <i class="fas fa-search"></i>
-        </button>
-    </form>`,
+            <input type="text" class="search-field" v-model.lazy="searchText">
+            <button @click.prevent="search()" class="btn-search" type="submit">
+                <i class="fas fa-search"></i>
+            </button>
+        </form>`,
     methods: {
         search() {
             this.$emit('search', this.searchText);
